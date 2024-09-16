8b42dc27866a779c9568d16045a034dde3ac53cf
shinichi04 <axelgiovanni888@gmail.com>
2024-09-16 17:43:38 +0800

    Add level 4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,9 @@
+import com.sun.source.util.TaskListener;
+
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Scanner;
-import  java.util.List;
+import java.util.List;
 
 public class Duke {
     public static void displayList(List<Task> anyList) {
@@ -22,7 +25,7 @@ public class Duke {
             if (prompt.equals("bye")) {
                 System.out.println(exitMsg);
             } else if (prompt.equals("list")) {
-                System.out.println("Here are your tasks in the list:\n");
+                System.out.println("Here are the tasks in your list:");
                 displayList(taskList);
             } else if (prompt.startsWith("mark")) {
                 String idxString = prompt.substring(5, 6);
@@ -36,9 +39,33 @@ public class Duke {
                 taskList.get(idxInt - 1).mark();
                 System.out.println("OK! I have marked this task as not done yet:\n"
                         + taskList.get(idxInt - 1).toString());
-            } else {
-                taskList.add(new Task(prompt));
-                displayList(taskList);
+            } else if (prompt.startsWith("todo")) {
+                String taskName = prompt.substring(5);
+                ToDo task = new ToDo(taskName);
+                taskList.add(task);
+                System.out.println("Got it. I've added this task:\n"
+                + task.toString() + "\nNow you have " + taskList.size() + " tasks in the list");
+            } else if (prompt.startsWith("deadline")) {
+                String imm1 = prompt.substring(9);
+                String[] partition = imm1.split("/by");
+                String taskName = partition[0];
+                String dueBy = partition[1];
+                Deadline task = new Deadline(taskName, dueBy);
+                taskList.add(task);
+                System.out.println("Got it. I've added this task:\n"
+                        + task.toString() + "\nNow you have " + taskList.size() + " tasks in the list");
+            } else if (prompt.startsWith("event")) {
+                String imm1 = prompt.substring(6);
+                String[] partition1 = imm1.split("/from");
+                String taskName = partition1[0];
+                String imm2 = partition1[1];
+                String[] partition2 = imm2.split("/to");
+                String from = partition2[0];
+                String to = partition2[1];
+                Event task = new Event(taskName, from, to);
+                taskList.add(task);
+                System.out.println("Got it. I've added this task:\n"
+                        + task.toString() + "\nNow you have " + taskList.size() + " tasks in the list");
             }
         }
     }
